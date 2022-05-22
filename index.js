dccd241e4a7122045e5da9ee9c0e8bb73d1d85df
unknown <tanjib.cse@gmail.com>
2022-05-22 20:35:24 +0600

    added tools get api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
@@ -8,12 +10,61 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.blvlf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+const run = async() => {
+
+    try {
+
+        await client.connect();
+        const toolsCollection = client.db("DeshMata").collection("tools");
+
+        app.get('/tools', async(req, res) => {
+            const query = {};
+            const result = await toolsCollection.find(query).toArray();
+            res.send(result);
+        })
+     
+
+    }
+
+
+    finally{
+        //await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Initial Setup Check
 app.get('/', (req, res) => {
     res.send("Hello From DeshMata Agronomic Tool & Manufacturing Inc.");
 })
-
 app.listen(port, () => {
     console.log('Listening on: ', port);
 })
