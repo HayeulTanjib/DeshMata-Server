e4e5990c78841b62e02f45ecd4e4ae3a780ebcc1
unknown <tanjib.cse@gmail.com>
2022-05-25 04:56:42 +0600

    profile and review section added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 
@@ -21,6 +22,9 @@ const run = async() => {
         await client.connect();
         const toolsCollection = client.db("DeshMata").collection("tools");
         const userCollection = client.db('DeshMata').collection("users");
+        const orderCollection = client.db('DeshMata').collection('orders');
+        const userProfileCollection = client.db('DeshMata').collection('profiles');
+        const reviewCollection = client.db('DeshMata').collection('reviews');
 
         //User
         app.put('/user/:email', async(req, res) => {
@@ -32,7 +36,10 @@ const run = async() => {
                 $set: user,
               };
             const result  = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result);
+
+            //JWT token initialize
+            const token = jwt.sign({ email: email }, process.env.JWT_TOKEN_SECRET, {expiresIn: '1d'})
+            res.send({result, token});
         })
 
         app.get('/tools', async(req, res) => {
@@ -49,9 +56,70 @@ const run = async() => {
             res.send(result);
         })
      
+        //Orders
+
+        app.post('/order', async(req, res) => {
+            const newOrder = req.body;
+            const result = await orderCollection.insertOne(newOrder);
+            res.send(result) 
+        })
+
+        app.get('/order/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email}
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/myorder/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //User Profile
+        app.put('/updateprofile/:email', async(req, res) => {
+            const email = req.params.email;
+            const updateProfile = req.body;
+            const filter = {email: email};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    education: updateProfile.education,
+                    location: updateProfile.location,
+                    phone: updateProfile.phone,
+                    linkedin: updateProfile.linkedin,
+                },
+            }
+            const result = await userProfileCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+        app.get('/profileinfo/:email', async(req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const result = await userProfileCollection.findOne(query)
+            res.send(result);
+        })
+
+        
+
+        //Review
+        app.post('/addreview', async(req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+        })
+
+        app.get('/allreview', async(req, res) => {
+            const query = {};
+            const result = await reviewCollection.find(query).toArray();
+            res.send(result);
+        })
+
 
     }
-
 
     finally{
         //await client.close();
